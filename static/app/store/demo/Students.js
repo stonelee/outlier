8f84e88dbf119af116f0bb1cdc16b6d742bb67a9
stonelee <istonelee@gmail.com>
2012-03-22 09:44:02 +0800

    store根据tastypie的要求更改参数，使得page控件可以使用

--- static/app/store/demo/Students.js
+++ static/app/store/demo/Students.js
@@ -1,11 +1,15 @@
 Ext.define('Outlier.store.demo.Students', {
 	extend: 'Ext.data.Store',
 	model: 'Outlier.model.demo.Student',
+	pageSize: 10,
 	proxy: {
 		type: 'rest',
 		url: '/demo/api/v1/student/',
+		pageParam: null,
+		startParam: 'offset',
 		reader: {
 			type: 'json',
+			totalProperty: 'meta.total_count',
 			root: 'objects'
 		}
 	}
